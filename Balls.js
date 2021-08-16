1476f0a2ce5bfc2af807d8901f392290a050c2d4
Pruthvi Shrikaanth <38621789+Blue0Tech@users.noreply.github.com>
2021-08-16 18:36:25 +0100

    Added pin count

--- Balls.js
+++ Balls.js
@@ -1,18 +1,23 @@
 AFRAME.registerComponent('balls',{
+    schema: {
+        handlingCollisions: {type:'bool',default:false},
+        ballThrown: {type:'bool',default:false}
+    },
     shootBullet: function() {
         window.addEventListener('keydown',e=>{
-            if(e.key=='z') {
-                var bullet = document.createElement('a-entity');
-                bullet.setAttribute('geometry',{
+            if(e.key=='z' && this.data.ballThrown==false) {
+                this.data.ballThrown=true;
+                var ball = document.createElement('a-entity');
+                ball.setAttribute('geometry',{
                     primitive: 'sphere',
                     radius: 0.35
                 });
-                bullet.setAttribute('material',{
+                ball.setAttribute('material',{
                     color: 'black'
                 });
                 var cam = document.querySelector('#camera');
                 var pos = cam.getAttribute('position');
-                bullet.setAttribute('position',pos);
+                ball.setAttribute('position',pos);
                 var camera = document.querySelector('#camera').object3D;
                 var direction = new THREE.Vector3();
                 camera.getWorldDirection(direction);
@@ -24,26 +29,45 @@ AFRAME.registerComponent('balls',{
                 //     z: cursorPos.z-pos.z
                 // };
                 // bullet.setAttribute('velocity',resultPos);
-                bullet.setAttribute('velocity',direction.multiplyScalar(-10));
+                ball.setAttribute('velocity',direction.multiplyScalar(-10));
                 var scene = document.querySelector('#scene');
-                bullet.setAttribute('dynamic-body',{
-                    mass: 30
+                ball.setAttribute('dynamic-body',{
+                    mass: 60
                 });
-                scene.appendChild(bullet);
+                ball.addEventListener('collide',this.handleCollision);
+                scene.appendChild(ball);
                 // cam.appendChild(bullet);
             };
         });
     },
+    handleCollision: function(e) {
+        var component = document.querySelector('#caller').getAttribute('balls');
+        if(component.handlingCollisions==false) {
+            var element = e.detail.target.el;
+            var elementHit = e.detail.body.el;
+            if(elementHit.id.includes('pin')) {
+                setTimeout(function() {
+                    var pins = document.querySelector('#pins');
+                    var pinsLeft = document.querySelector('#pinsLeft');
+                    for(var child of pins.children) {
+                        var position = child.getAttribute('position');
+                        var id = child.getAttribute('id');
+                        if(position.y<0) {
+                            child.remove();
+                        }
+                    };
+                    pinsLeft.setAttribute('text',{
+                        value: `${pins.children.length} pins left`
+                    });
+                    component.handlingCollisions = false;
+                    component.ballThrown = false;
+                },3000);
+                element.removeEventListener('collide',this.handleCollision);
+                component.handlingCollisions = true;
+            }
+        }
+    },
     init: function() {
         this.shootBullet();
-    },
-    // tick: function() {
-    //     var camera = document.querySelector('#camera');
-    //     var rotation = camera.getAttribute('rotation');
-    //     rotation.y+=180;
-    //     rotation.x*=-1;
-    //     var gun = document.querySelector('#gunEntity');
-    //     gun.setAttribute('rotation',rotation);
-    //     gun.setAttribute('position',camera.getAttribute('position'));
-    // }x
+    }
 });
